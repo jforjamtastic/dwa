8144ac47c19e0f3ffc4cede4499f6b42ab05320e
Ben Wu <benwu@libra-magnetica.home>
2012-10-17 21:52:54 -0400

    Try 2

---
+++ p2.benwu.me/controllers/c_users.php
@@ -0,0 +1,94 @@
+<?php
+
+class users_controller extends base_controller {
+	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function index() {
+		echo "welcome to the users department";
+	}
+	
+	public function signup() {
+		$this->template->content = View::instance("v_users_signup");
+		
+		echo $this->template;
+	}
+	
+	public function p_signup(){
+		#what data was printed
+		print_r($_POST);
+		
+				
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']); #hashes password
+		$_POST['created'] = Time::now(); #this returned the time stamp
+		$_POST['modified']= Time::now(); #timestamp 
+		$_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
+		
+		#put the data into db
+		DB::instance(DB_NAME)->insert('users', $_POST); #inserts form data to DB
+		
+		echo "You are Registered";
+		
+		
+	}
+	
+	public function login() {
+		$this->template->content = View::instance("v_users_login");
+		
+		echo $this->template;	
+	}
+	
+	public function p_login(){
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+		
+		$q = "SELECT token
+				FROM users
+				WHERE email = '".$_POST['email']."'
+				AND password = '".$_POST['password']."'
+				";
+				
+		$token = DB::instance(DB_NAME)->select_field($q);
+		
+		if ($token == ""){
+			Router::redirect("/users/login");
+		}
+		else{
+			setcookie("token", $token, strtotime('+2 weeks'), "/");
+			Router::redirect("/");
+			
+		}
+	
+	}
+	
+	public function logout() {
+		echo "display the logout page";
+	}
+	
+	public function profile($user_name){
+		if(!$this->user){
+			echo "Members only";
+			return false;
+		}
+		
+		if ($user_name == NULL) {
+			echo "you did not specify a user";
+		} 
+		else {		
+			# Create the view
+			$this->template->content = View::instance("v_users_profile");
+			$this->template->title = "Profile for ".$user_name;
+			
+			# Add client files
+			$client_files = Array("/css/users.css", "/js/users.js");
+			$this->template->client_files = Utils::load_client_files($client_files);
+			
+			# Pass Content
+			$this->template->content->user_name = $user_name;
+			
+			# Render the View
+			echo $this->template;
+		}
+	}	
+}
---
+++ p2.benwu.me/views/v_users_login.php
@@ -0,0 +1,12 @@
+<form method='Post' action='/users/p_login'>
+
+	email<br />
+	<input type='text' name='email'>
+	<br /><br />
+	password<br />
+	<input type='password' name='password'>
+	<br /><br />
+	
+	<input type='submit'>
+
+</form>
