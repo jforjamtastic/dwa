2a769f7f5a07812cee7f5ff0eb8649abd0f97b27
Ben Wu <benwu@libra-magnetica.home>
2012-10-23 22:25:00 -0400

    Updating framework

--- p2.benwu.me/controllers/c_users.php
+++ p2.benwu.me/controllers/c_users.php
@@ -13,7 +13,7 @@ class users_controller extends base_controller {
 	public function signup() {
 		$this->template->content = View::instance("v_users_signup");
 		
-		$client_files = Array("/css/style.css", "");
+		$client_files = Array("/css/style.css", "http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js");
 		$this->template->client_files = Utils::load_client_files($client_files);
 				
 		echo $this->template;
--- p2.benwu.me/views/v_users_signup.php
+++ p2.benwu.me/views/v_users_signup.php
@@ -6,19 +6,24 @@
 	</ul>
 </div>
 
-<form method='Post' action='/users/p_signup'>
-	First Name<br />
-	<input type='text' name='first_name'><br /><br />
-	
-	Last Name<br />
-	<input type='text' name='last_name'><br /><br />
-	
-	Email<br />
-	<input type='text' name='email'><br /><br />
-	
-	Password<br />
-	<input type='password' name='password'><br /><br />
-	
-	<input type='submit'><br /><br />
+<div id="content">
+	<form method='Post' action='/users/p_signup'>
+		<fieldset>
+			<h2>Signup</h2>
+			<label for='first_name'>First Name</label><br />
+			<input type='text' name='first_name' id='first_name'><br /><br />
+		
+			<label for='last_name'>Last Name</label><br />
+			<input type='text' name='last_name' id='last_name'><br /><br />
+			
+			<label for='email'>email</label><br />
+			<input type='text' name='email' id='email'><br /><br />
+			
+			<label for='password'>Password</label><br />
+			<input type='password' name='password' id='password'><br /><br />
+			
+			<input type='submit' name="submit"><br /><br />
+		</fieldset>
 
-</form>
+		</form>
+</div>
