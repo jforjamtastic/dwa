fdca9549c8cbdd95e27b9215f7ea361d30c84514
Ben Wu <benwu@libra-magnetica.home>
2012-11-01 13:53:11 -0400

    working on css

--- p2.benwu.me/views/v_posts_index.php
+++ p2.benwu.me/views/v_posts_index.php
@@ -1,8 +1,11 @@
 <? foreach($posts as $key => $post): ?>
 	<div class='post'>
-		<?=$post['first_name']?> <?=$post['last_name']?> posted:
-		<?=$post['content']?>
-	
+		<div class="post-background"></div>
+		<div class="post-text">
+			<span><?=$post['first_name']?> <?=$post['last_name']?></span>
+			<br />
+			<?=$post['content']?>
+		</div>
 	</div>
 	
 <? endforeach; ?>
